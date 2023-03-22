f65cbd15fadc59beaf9e179a658219e7d7d2a6dd
Muflih <69343587+mufmufmuf@users.noreply.github.com>
2023-03-22 20:56:56 +0700

    fade & animation adjustments

--- more/more.js
+++ more/more.js
@@ -24,3 +24,18 @@ $(".main-gallery").flickity({
     cellAlign: "left",
     contain: true,
 });
+
+/* FadeIn Scroll */
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $(".fade").each(function (i) {
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if (bottom_of_window > bottom_of_object) {
+                $(this).animate({ opacity: "1" }, 900);
+            }
+        });
+    });
+});
